fda96489404a34212d50f48de041b7b3b9e0dae9
Ade <53797920+aakano684@users.noreply.github.com>
2020-10-31 14:43:17 +0000

    Update registerNewUser.js

--- mathdojo-nonproduction/rules/registerNewUser.js
+++ mathdojo-nonproduction/rules/registerNewUser.js
@@ -1,13 +1,15 @@
 function registerNewUser(user, context, callback) {
   const axios = require('axios@0.19.2');
   const options = { method: 'POST',
-    url: `https://${configuration.userAccountServiceDomain}/organisations/{organisationId}/users`,
+    url: `https://${configuration.userAccountServiceDomain}/organisations/user.organisationId/users`,
     headers: { 'content-type': 'application/json' },
     data: `{"name":user.name}` };
 
   axios(options)
     .then( res => {
- 	context.idToken['permissions'] = res.data.permissions;
+      	const access_token = 
+    	console.log(res.data);
+ 	context.idToken['user.permissions'] = res.data.permissions;
     })
     .catch( err => {
       return err;
