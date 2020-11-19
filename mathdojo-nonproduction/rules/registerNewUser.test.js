e57271dce38355257a543438d30e00f2c27f589d
noce2 <20339399+noce2@users.noreply.github.com>
2020-11-19 09:20:00 +0000

    Use describe to share functionality in tests

--- mathdojo-nonproduction/rules/registerNewUser.test.js
+++ mathdojo-nonproduction/rules/registerNewUser.test.js
@@ -2,34 +2,41 @@ const auth0RuleLoader = require("../../testing/helpers").loadAuth0Rule;
 const registerNewUserCodeLocation =
   "mathdojo-nonproduction/rules/registerNewUser.js";
 
-test("the rule calls axios", () => {
-  // Given
-  const mockAxios = jest
-    .fn()
-    .mockName("mockAxios")
-    .mockResolvedValue({
-      data: {
-        permissions: [],
-      },
+describe("Register New User Rule successfully calls User Account Service", () => {
+  const mockAxios = jest.fn().mockName('mockAxios');
+  const mockAuth0Callback = jest.fn().mockName('mockAuth0Callback');
+
+  beforeEach(() => {
+    mockAxios.mockClear();
+    mockAuth0Callback.mockClear();
+  })
+
+  test("the rule calls axios", () => {
+    // Given
+      mockAxios.mockResolvedValue({
+        data: {
+          permissions: [],
+        },
+      });
+  
+    const mapOfModulesToOverride = new Map();
+    mapOfModulesToOverride["axios@0.19.2"] = mockAxios;
+  
+    const auth0ConfigurationObject = {
+      userAccountServiceDomain: "localdomain",
+    };
+  
+    const registerNewUserFunction = auth0RuleLoader({
+      ruleLocation: registerNewUserCodeLocation,
+      mapOfRequiredModulesToReplaceWithMocks: mapOfModulesToOverride,
+      configuration: auth0ConfigurationObject,
     });
-
-  const mapOfModulesToOverride = new Map();
-  mapOfModulesToOverride["axios@0.19.2"] = mockAxios;
-
-  const mockAuth0Callback = jest.fn();
-  const auth0ConfigurationObject = {
-    userAccountServiceDomain: "localdomain",
-  };
-
-  const registerNewUserFunction = auth0RuleLoader({
-    ruleLocation: registerNewUserCodeLocation,
-    mapOfRequiredModulesToReplaceWithMocks: mapOfModulesToOverride,
-    configuration: auth0ConfigurationObject,
+  
+    // When
+    registerNewUserFunction({}, {}, mockAuth0Callback);
+  
+    // Then
+    expect(mockAuth0Callback.mock.calls.length).toBe(3);
   });
 
-  // When
-  registerNewUserFunction({}, {}, mockAuth0Callback);
-
-  // Then
-  expect(mockAuth0Callback.mock.calls.length).toBe(3);
 });
