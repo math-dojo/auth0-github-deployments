9cf22bb257b5b1774592fe2f1dd6e57783bbb34c
noce2 <20339399+noce2@users.noreply.github.com>
2020-11-19 09:15:37 +0000

    Change global mock clear to false

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // cacheDirectory: "C:\\Users\\essie\\AppData\\Local\\Temp\\jest",
 
   // Automatically clear mock calls and instances between every test
-  clearMocks: true,
+  clearMocks: false,
 
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
