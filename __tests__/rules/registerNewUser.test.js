932b897ca1d41d0a607fefa63b2dd132b9db0b59
noce2 <20339399+noce2@users.noreply.github.com>
2020-11-26 07:32:47 +0000

    Add explanation for test setup

--- __tests__/rules/registerNewUser.test.js
+++ __tests__/rules/registerNewUser.test.js
@@ -1,42 +1,43 @@
-const auth0RuleLoader = require('../helpers/function-loader').loadAuth0Rule;
+const auth0RuleLoader = require("../helpers/function-loader").loadAuth0Rule;
 
 const registerNewUserCodeLocations = [
-  ['Nonproduction', 'mathdojo-nonproduction/rules/registerNewUser.js'],
+  ["Nonproduction", "mathdojo-nonproduction/rules/registerNewUser.js"],
 ];
-const mathDojoNamespace = 'http://math-dojo.io/';
+const mathDojoNamespace = "http://math-dojo.io/";
 
 jest.useFakeTimers();
 
 describe.each(registerNewUserCodeLocations)(
-  '%s Register New User Rule successfully calls User Account Service',
+  "%s Register New User Rule successfully calls User Account Service",
   (_environment, registerNewUserCodeLocation) => {
-    const mockAxios = jest.fn().mockName('mockAxios');
-    const mockAuth0Callback = jest.fn().mockName('mockAuth0Callback');
+    const mockAxios = jest.fn().mockName("mockAxios");
+    const mockAuth0Callback = jest.fn().mockName("mockAuth0Callback");
 
     beforeEach(() => {
       mockAxios.mockClear();
       mockAuth0Callback.mockClear();
     });
 
-    test('the rule waits for the axios resolution before calling callback', () => {
+    test("the rule waits for the axios resolution before calling callback", () => {
       // Given
       mockAxios.mockImplementation(
-        () => new Promise((resolveFunction) => {
-          setTimeout(() => {
-            resolveFunction({
-              data: {
-                permissions: [],
-              },
-            });
-          }, 5000);
-        }),
+        () =>
+          new Promise((resolveFunction) => {
+            setTimeout(() => {
+              resolveFunction({
+                data: {
+                  permissions: [],
+                },
+              });
+            }, 5000);
+          })
       );
 
       const mapOfModulesToOverride = new Map();
-      mapOfModulesToOverride['axios@0.19.2'] = mockAxios;
+      mapOfModulesToOverride["axios@0.19.2"] = mockAxios;
 
       const auth0ConfigurationObject = {
-        userAccountServiceDomain: 'localdomain',
+        userAccountServiceDomain: "localdomain",
       };
 
       const registerNewUserFunction = auth0RuleLoader({
@@ -51,20 +52,34 @@ describe.each(registerNewUserCodeLocations)(
       // Then
       expect(mockAuth0Callback).not.toHaveBeenCalled();
 
-    /**
-   * This makes the setTtimeout execute instanteously for the purposes
-   * of the test, accelerating the flow of virtual time.
-   */
+      /**
+       * This makes the setTtimeout execute instanteously for the purposes
+       * of the test, accelerating the flow of virtual time.
+       */
       jest.runAllTimers();
 
-      return Promise.resolve().then(() => Promise.all([
-        expect(mockAuth0Callback).toHaveBeenCalledTimes(1),
-        expect(mockAuth0Callback).toHaveBeenCalledWith(null, {}, {
-          idToken: {
-            [`${mathDojoNamespace}user_permissions`]: [],
-          },
-        }),
-      ]));
+      /**
+       * The previous execution of setTimeout put the promise returning data
+       * at the front of the Node.js Job Queue. The test now needs to wait
+       * for the next "tick" of the event queue to check whether the auth0 callback
+       * was used. The simple way to do that is to perform the assertions after
+       * the resolution of another promise, which will now sit behind the one
+       * already queued by setTimeout.
+       */
+      return Promise.resolve().then(() =>
+        Promise.all([
+          expect(mockAuth0Callback).toHaveBeenCalledTimes(1),
+          expect(mockAuth0Callback).toHaveBeenCalledWith(
+            null,
+            {},
+            {
+              idToken: {
+                [`${mathDojoNamespace}user_permissions`]: [],
+              },
+            }
+          ),
+        ])
+      );
     });
-  },
+  }
 );
